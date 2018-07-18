4eb0a85a4055b3b6c5fa79ac28705e11c197537a
yousof <y.ghahremani@gmail.com>
2018-07-18 10:50:00 -0400

    Only offer actions the platform will actually accept
    Filtered by what the state machine allows and by what the token allows.
    The fraud rule is the one worth noting: a line suspended for fraud does
    not resume because somebody clicked Resume, so the button only appears
    for someone who can be the approver the API demands.

--- src/app/sims/sim-presentation.spec.ts
+++ src/app/sims/sim-presentation.spec.ts
@@ -1,5 +1,6 @@
 import { Sim, SimState } from './sim.model';
-import { labelFor, badgeFor, isTransitional } from './sim-presentation';
+import { labelFor, badgeFor, isTransitional, availableActions } from './sim-presentation';
+import { Scopes } from '../core/permissions';
 
 function sim(state: SimState, extra: Partial<Sim> = {}): Sim {
   return {
@@ -14,6 +15,10 @@ function sim(state: SimState, extra: Partial<Sim> = {}): Sim {
     ...extra
   };
 }
+
+const AGENT = [Scopes.READ_SIM, Scopes.ACTIVATE_SIM, Scopes.SUSPEND_SIM, Scopes.TERMINATE_SIM];
+const READ_ONLY = [Scopes.READ_SIM];
+const RISK = AGENT.concat([Scopes.APPROVE_FRAUD]);
 
 describe('labels and badges', () => {
   it('gives every state a human label', () => {
@@ -34,5 +39,51 @@ describe('labels and badges', () => {
     expect(isTransitional('TERMINATING')).toBe(true);
     expect(isTransitional('ACTIVE')).toBe(false);
     expect(isTransitional('SUSPENDED')).toBe(false);
+  });
+});
+
+describe('available actions', () => {
+  it('offers suspend and terminate on a live line', () => {
+    expect(availableActions(sim('ACTIVE'), AGENT).sort()).toEqual(['SUSPEND', 'TERMINATE']);
+  });
+
+  it('offers resume on a suspended line', () => {
+    const s = sim('SUSPENDED', { suspensionReason: 'NON_PAYMENT' });
+    expect(availableActions(s, AGENT).sort()).toEqual(['RESUME', 'TERMINATE']);
+  });
+
+  it('offers nothing at all while the carrier has it', () => {
+    expect(availableActions(sim('ACTIVATING'), AGENT)).toEqual([]);
+    expect(availableActions(sim('TERMINATING'), AGENT)).toEqual([]);
+  });
+
+  it('offers nothing on a dead line', () => {
+    expect(availableActions(sim('TERMINATED'), AGENT)).toEqual([]);
+  });
+
+  it('offers activate and terminate from inventory', () => {
+    expect(availableActions(sim('PRE_ACTIVE'), AGENT).sort()).toEqual(['ACTIVATE', 'TERMINATE']);
+  });
+
+  it('gives a read-only user no buttons', () => {
+    expect(availableActions(sim('ACTIVE'), READ_ONLY)).toEqual([]);
+  });
+
+  it('hides resume on a fraud suspension from an ordinary agent', () => {
+    const s = sim('SUSPENDED', { suspensionReason: 'FRAUD' });
+
+    expect(availableActions(s, AGENT)).not.toContain('RESUME');
+    // Terminate is still on the table - fraud lines still get torn down.
+    expect(availableActions(s, AGENT)).toContain('TERMINATE');
+  });
+
+  it('shows resume on a fraud suspension to someone who can approve it', () => {
+    const s = sim('SUSPENDED', { suspensionReason: 'FRAUD' });
+    expect(availableActions(s, RISK)).toContain('RESUME');
+  });
+
+  it('lets admin do anything the state allows', () => {
+    const s = sim('SUSPENDED', { suspensionReason: 'FRAUD' });
+    expect(availableActions(s, [Scopes.ADMIN]).sort()).toEqual(['RESUME', 'TERMINATE']);
   });
 });
--- src/app/sims/sim-presentation.ts
+++ src/app/sims/sim-presentation.ts
@@ -1,9 +1,12 @@
-import { SimState } from './sim.model';
+import { Sim, SimAction, SimState } from './sim.model';
+import { Scopes, hasScope } from '../core/permissions';
 
 /**
  * How a SIM state is shown, and what may be done to it.
  *
- * Labels and badges only for now; which actions are offered comes next.
+ * The transitions here deliberately mirror the platform's state machine. If
+ * the two ever disagree the portal offers a button that returns 409, so this
+ * table is the front end's half of a contract and is tested as such.
  */
 
 const LABELS: { [K in SimState]: string } = {
@@ -28,6 +31,22 @@ const BADGES: { [K in SimState]: string } = {
   TERMINATED: 'badge--dead'
 };
 
+const TRANSITIONS: { [K in SimState]: SimAction[] } = {
+  PRE_ACTIVE: ['ACTIVATE', 'TERMINATE'],
+  ACTIVATING: [],
+  ACTIVE: ['SUSPEND', 'TERMINATE'],
+  SUSPENDED: ['RESUME', 'TERMINATE'],
+  TERMINATING: [],
+  TERMINATED: []
+};
+
+const REQUIRED_SCOPE: { [K in SimAction]: string } = {
+  ACTIVATE: Scopes.ACTIVATE_SIM,
+  SUSPEND: Scopes.SUSPEND_SIM,
+  RESUME: Scopes.SUSPEND_SIM,
+  TERMINATE: Scopes.TERMINATE_SIM
+};
+
 export function labelFor(state: SimState): string {
   return LABELS[state] || state;
 }
@@ -42,4 +61,28 @@ export function badgeFor(state: SimState): string {
  */
 export function isTransitional(state: SimState): boolean {
   return state === 'ACTIVATING' || state === 'TERMINATING';
+}
+
+/**
+ * What this agent may actually do to this SIM.
+ *
+ * Two filters: what the state machine permits, and what the token permits.
+ * The fraud rule is the interesting one - a line suspended for fraud does not
+ * come back because somebody clicked Resume. The API requires an approver on
+ * that transition, so the portal only offers it to someone who can be one.
+ */
+export function availableActions(sim: Sim, heldScopes: string[]): SimAction[] {
+  const byState = TRANSITIONS[sim.state] || [];
+
+  return byState.filter(action => {
+    if (!hasScope(heldScopes, REQUIRED_SCOPE[action])) {
+      return false;
+    }
+
+    if (action === 'RESUME' && sim.suspensionReason === 'FRAUD') {
+      return hasScope(heldScopes, Scopes.APPROVE_FRAUD);
+    }
+
+    return true;
+  });
 }
